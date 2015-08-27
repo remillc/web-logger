208592d5ddfe96abc56f1b1b96c6e50774e26f19
remillc <christian.remillard@gmail.com>
2015-08-27 11:40:42 -0400

    typo

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-﻿'use strict';
+﻿
+'use strict';
 
-var FileStreamRotator 	= require('file-stream-rotator'),
-	fs 						= require('fs'),
-	morgan 					= require('morgan'),
-	moment					= require('moment'),
-	extend					= require('extend'),
+var FileStreamRotator = require('file-stream-rotator'),
+	fs = require('fs'),
+	morgan = require('morgan'),
+	moment = require('moment'),
+	extend = require('extend'),
 
-	defaults					= {
+	defaults = {
 		logDirectory: './logs'
 	};
 
 // setup the logger
-module.exports = function(format, options){
+module.exports = function(format, options) {
 
 	options = extend(true, (options || {}), defaults);
 
@@ -20,33 +21,29 @@ module.exports = function(format, options){
 
 	// create a rotating write stream
 	var accessLogStream = FileStreamRotator.getStream({
-	  filename: options.logDirectory + '/access-%DATE%.log',
-	  frequency: 'daily',
-	  verbose: false,
-	  date_format: "YYYY-MM-DD"
+		filename: options.logDirectory + '/access-%DATE%.log',
+		frequency: 'daily',
+		verbose: false,
+		date_format: "YYYY-MM-DD"
 	});
 
-	morgan.token('remote-addr', function(req, res){
-		return req.headers['x-forwarded-for']
-			|| req.ip
-			|| req._remoteAddress
-			|| (req.connection && req.connection.remoteAddress)
-			|| undefined;
+	morgan.token('remote-addr', function(req, res) {
+		return req.headers['x-forwarded-for'] || req.ip || req._remoteAddress || (req.connection && req.connection.remoteAddress) || undefined;
 	});
 
-	morgan.token('date', function(req, res){
+	morgan.token('date', function(req, res) {
 		// %d/%b/%Y:%H:%M:%S %z
 		return moment().format('DD/MMM/YYYY:HH:mm:ss ZZ');
 	});
 
-	return function(req, res, next){
-			morgan('combined', {
-				stream: accessLogStream,
-				skip: function (req, res){
-					//Don't log Nagios requests
-					return req.headers['user-agent'] && req.headers['user-agent'].indexOf('nagios-plugins') > -1 && req.headers['x-forwarded-for'] && req.headers['x-forwarded-for'].indexOf('10.') == 0;
+	return function(req, res, next) {
+		morgan('combined', {
+			stream: accessLogStream,
+			skip: function(req, res) {
+				//Don't log Nagios requests
+				return req.headers['user-agent'] && req.headers['user-agent'].indexOf('nagios-plugins') > -1 && req.headers['x-forwarded-for'] && req.headers['x-forwarded-for'].indexOf('10.') == 0;
 			}
-			next();
 		});
+		next();
 	};
 };
