c4f38cebd5a63c96681b42f0fe5089f96795a272
remillc <christian.remillard@gmail.com>
2015-08-27 12:13:58 -0400

    a

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 ﻿
 'use strict';
 
-var FileStreamRotator = require('file-stream-rotator'),
-	fs = require('fs'),
-	morgan = require('morgan'),
-	moment = require('moment'),
-	extend = require('extend'),
+var FileStreamRotator 	= require('file-stream-rotator'),
+	fs 						= require('fs'),
+	morgan 					= require('morgan'),
+	moment 					= require('moment'),
+	extend 					= require('extend'),
 
 	defaults = {
 		format: 'combined',
@@ -27,14 +27,6 @@ module.exports = function(options) {
 	// ensure log directory exists
 	fs.existsSync(settings.logDirectory) || fs.mkdirSync(settings.logDirectory)
 
-	// create a rotating write stream
-	var accessLogStream = FileStreamRotator.getStream({
-		filename: settings.logDirectory + '/access-%DATE%.log',
-		frequency: 'daily',
-		verbose: false,
-		date_format: "YYYY-MM-DD"
-	});
-
 	morgan.token('remote-addr', function(req, res) {
 		return req.headers['x-forwarded-for'] || req.ip || req._remoteAddress || (req.connection && req.connection.remoteAddress) || undefined;
 	});
@@ -44,14 +36,20 @@ module.exports = function(options) {
 		return moment().format('DD/MMM/YYYY:HH:mm:ss ZZ');
 	});
 
-	return function(req, res, next) {
-		morgan(settings.format, {
-			stream: accessLogStream,
-			skip: function(req, res) {
-				//Don't log Nagios requests
-				return req.headers['user-agent'] && req.headers['user-agent'].indexOf('nagios-plugins') > -1 && req.headers['x-forwarded-for'] && req.headers['x-forwarded-for'].indexOf('10.') == 0;
-			}
-		});
-		next();
-	};
+	// create a rotating write stream
+	var accessLogStream = FileStreamRotator.getStream({
+		filename: settings.logDirectory + '/access-%DATE%.log',
+		frequency: 'daily',
+		verbose: false,
+		date_format: "YYYY-MM-DD"
+	});
+
+	return morgan(settings.format, {
+		stream: accessLogStream,
+		skip: function(req, res) {
+			//Don't log Nagios requests
+			return req.headers['user-agent'] && req.headers['user-agent'].indexOf('nagios-plugins') > -1 && req.headers['x-forwarded-for'] && req.headers['x-forwarded-for'].indexOf('10.') == 0;
+		}
+	});
+
 };
